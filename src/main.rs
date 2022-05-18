90d1d4c1b8a1381dd4a289fb2c8e8cdbb7e7e519
Silen Locatelli <privat@silenlocatelli.ch>
2022-05-18 15:49:36 +0000

    added key send lib

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
+use rdev::{simulate, Button, EventType, Key, SimulateError};
+use std::{thread, time};
+
+fn send(event_type: &EventType) {
+    let delay = time::Duration::from_millis(20);
+    match simulate(event_type) {
+        Ok(()) => (),
+        Err(SimulateError) => {
+            println!("We could not send {:?}", event_type);
+        }
+    }
+    // Let ths OS catchup (at least MacOS)
+    thread::sleep(delay);
+}
+
+
+
+
+
 
 fn main() {
+
+send(&EventType::KeyPress(Key::KeyS));
+send(&EventType::KeyRelease(Key::KeyS));
+
     println!("Hello World")
 }
+
+
