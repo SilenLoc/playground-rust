1353cdeb1120dd1bd33b1b158f49098118223db2
Silen Locatelli <88315530+SilenLoc@users.noreply.github.com>
2022-06-01 13:31:23 +0200

    added a keyboard simulation functionality

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,17 @@
-use rdev::{simulate, Button, EventType, Key, SimulateError};
+use keyboard::type_word;
 use std::{thread, time};
 
-fn send(event_type: &EventType) {
-    let delay = time::Duration::from_millis(20);
-    match simulate(event_type) {
-        Ok(()) => (),
-        Err(SimulateError) => {
-            println!("We could not send {:?}", event_type);
-        }
-    }
-    // Let ths OS catchup (at least MacOS)
-    thread::sleep(delay);
-}
 
 
-
-
-
+pub mod keyboard;
 
 fn main() {
-
-send(&EventType::KeyPress(Key::KeyS));
-send(&EventType::KeyRelease(Key::KeyS));
-
-    println!("Hello World")
+    let delay = time::Duration::from_millis(1000);
+    thread::sleep(delay);
+    type_word("some Word")
 }
+
+
+
 
 
