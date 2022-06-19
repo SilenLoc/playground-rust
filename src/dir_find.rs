5fb97439f3d595c2d7ef41430b8f43ae9bbaba98
SilenLoc <private@locatelli.ch>
2022-06-19 13:46:12 +0200

    using directly dir

--- src/dir_find.rs
+++ src/dir_find.rs
@@ -2,14 +2,13 @@ use std::fs;
 use std::path::PathBuf;
 
 pub fn find_dir(asked_dir_path: String) -> PathBuf {
-    let out_dir: String = asked_dir_path.clone();
 
-    for entry in fs::read_dir(&out_dir).unwrap() {
+    for entry in fs::read_dir(&asked_dir_path).unwrap() {
         let path = entry.unwrap().path();
-        println!("content of {} {} {}", &out_dir, ": ", path.file_name().unwrap().to_str().unwrap());
+        println!("content of {} {} {}", &asked_dir_path, ": ", path.file_name().unwrap().to_str().unwrap());
     }
 
-    let path_entry = fs::read_dir(&out_dir);
+    let path_entry = fs::read_dir(&asked_dir_path);
     let path_buf = path_entry.unwrap().last().unwrap().unwrap().path();
 
     return path_buf;
