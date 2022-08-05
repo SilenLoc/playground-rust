10e22d60a76fc06124bf4215dad2e2184d7442b5
Silen Locatelli <88315530+SilenLoc@users.noreply.github.com>
2022-08-05 11:37:08 +0200

    add logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate core;
 
+use crate::logging::init_logger;
 use crate::persistence::{env_default, PersistenceEnv};
 use crate::persistence::disk_pers::{create_or_load_map_env};
 use crate::run_tool::run_tool;
@@ -7,8 +8,11 @@ use crate::run_tool::run_tool;
 mod run_tool;
 mod command_runner;
 mod persistence;
+mod logging;
 
 fn main() {
+    init_logger().expect("could not init logger");
+
     let mut disk_map = create_or_load_map_env("test.json".to_string());
 
     disk_map.put("hello".to_string(), "world".to_string());
