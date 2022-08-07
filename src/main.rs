e3e34d921d82f1d820688c8e6e020af199566947
SilenLoc <private@locatelli.ch>
2022-08-07 21:30:59 +0200

    *smallest gui

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,31 @@
-extern crate core;
-
-use crate::logging::init_logger;
-use crate::persistence::{env_default, PersistenceEnv};
-use crate::persistence::disk_pers::{create_or_load_map_env};
-use crate::run_tool::run_tool;
-
 mod run_tool;
-mod command_runner;
-mod persistence;
 mod logging;
+mod command_runner;
+
+use eframe::egui;
 
 fn main() {
-    init_logger().expect("could not init logger");
-
-    let mut disk_map = create_or_load_map_env("test.json".to_string());
-
-    disk_map.put("hello".to_string(), "world".to_string());
-
-    let result = disk_map.get("hello".to_string());
-
-    println!("{}", result.unwrap())
-
-
-
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("EasRun", native_options, Box::new(|cc| Box::new(MyEguiApp::new(cc))));
 }
 
+#[derive(Default)]
+struct MyEguiApp {}
 
+impl MyEguiApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        Self::default()
+    }
+}
 
-
-
+impl eframe::App for MyEguiApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello World!");
+        });
+    }
+}
